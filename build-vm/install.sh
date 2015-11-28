5f34f038ea83a0380a66d34ac06eef538e7ef6b3
Akriti Tripathi <atripat4@ncsu.edu>
2015-11-28 17:15:40 -0500

    updating vm-contents

--- build-vm/install.sh
+++ build-vm/install.sh
@@ -1,11 +1,14 @@
+#installing packages 
 sudo apt-get update
 sudo apt-get install -y openjdk-7-jre git 
-
+# cloning git repo for vm contents
 git clone https://github.com/SoftwareEngineeringToolDemos/FSE-2014-OMEN-plus.git /home/vagrant/Desktop/FSE-2014-OMEN-plus
-
+# moving files to desktop
 mv /home/vagrant/Desktop/FSE-2014-OMEN-plus/build-vm/vm-contents/* /home/vagrant/Desktop/
-
 rm -rf /home/vagrant/Desktop/FSE-2014-OMEN-plus
+#creating upstart job for configuring sidebar and tool visibility(asciinema)
+mv /home/vagrant/Desktop/omen.conf /home/vagrant/.config/upstart/omen.conf
+#rebooting once to activate upstart jobs
+reboot
 
-#xdg-open https://asciinema.org/a/6fz06vggoj5y09yl6gfbx6uti
-#DISPLAY=:0 nohup /usr/bin/firefox -new-window https://asciinema.org/a/6fz06vggoj5y09yl6gfbx6uti & 2> /dev/null;
+
